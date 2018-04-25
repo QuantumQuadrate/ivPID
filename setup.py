b38fd9111c600d304be8277d7a0a9c7d0b909bcc
Matt Ebert <mfe5003@gmail.com>
2018-04-24 21:40:33 -0500

    added setup script and changed layout

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='ivPID',
+    version='1.0',
+    description='PID controller',
+    url='https://github.com/QuantumQuadrate/ivPID',
+    packages=find_packages(),
+    license='GPL3',
+)
