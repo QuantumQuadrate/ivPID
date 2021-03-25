a9fbfdfcdff4ba00a7205058c03384435f4d653c
paul-romlow <59883832+paul-romlow@users.noreply.github.com>
2021-03-25 11:23:19 -0500

    Update PID.py

--- ivPID/PID.py
+++ ivPID/PID.py
@@ -36,7 +36,7 @@ class PID:
     """PID Controller
     """
 
-    def __init__(self, P=0.2, I=0.0, D=0.0, invert=False, windup=0.0):
+    def __init__(self, P=0.2, I=0.0, D=0.0, invert=False, windup=0.0, set_point=0.0):
         self.Kp = P
         self.Ki = I
         self.Kd = D
@@ -46,7 +46,8 @@ class PID:
         self.sample_time = 0.00
         self.current_time = time.time()
         self.last_time = self.current_time
-
+        self.SetPoint = set_point
+        
         self.clear()
 
     def clear(self):
@@ -61,7 +62,6 @@ class PID:
         self.windup_guard = self.windup
 
         self.output = 0.0
-        self.SetPoint = 0.0
 
     def update(self, feedback_value):
         """Calculates PID value for given reference feedback
